15aa065a9e6963825f341fa5a7de09d608e660e9
Mundoinformatica Canaria <mundoinformaticacanaria@gmail.com>
2026-08-21 12:27:04 +0100

    Fix Room backup test SDK configuration

--- app/src/test/java/com/mundoinformaticacanaria/gymup/data/backup/RoomBackupDataSourceTest.kt
+++ app/src/test/java/com/mundoinformaticacanaria/gymup/data/backup/RoomBackupDataSourceTest.kt
@@ -27,8 +27,10 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class RoomBackupDataSourceTest {
     private lateinit var context: Context
     private lateinit var database: GymUpDatabase
